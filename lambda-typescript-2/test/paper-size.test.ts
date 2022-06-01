b9f05bea9ff42e88a834999e8d2fbf6fe4ac3c56
Johannes Koch <johanneskoch@fico.com>
2022-06-01 06:48:42 +0200

    Change tests

--- lambda-typescript-2/test/paper-size.test.ts
+++ lambda-typescript-2/test/paper-size.test.ts
@@ -3,15 +3,15 @@ import * as PaperSize from '../lib/paper-size';
 
 test('PaperSizeInGermany', () => {
     expect.assertions(1);
-    expect(PaperSize.handler({Country: "Germany"})).resolves.toEqual("A4");
-    });
+    expect(PaperSize.handler({ Country: "Germany" })).resolves.toEqual("A4");
+});
 
-    test('PaperSizeInUSA', () => {
-        expect.assertions(1);
-        expect(PaperSize.handler({Country: "USA"})).resolves.toEqual("Letter");
-        });
+test('PaperSizeInUSA', () => {
+    expect.assertions(1);
+    expect(PaperSize.handler({ Country: "USA" })).resolves.toEqual("Letter");
+});
 
-        test('PaperSizeInIndia', () => {
-            expect.assertions(1);
-            expect(PaperSize.handler({Country: "India"})).resolves.toEqual("Unknown");
-            });
+test('PaperSizeInIndia', () => {
+    expect.assertions(1);
+    expect(PaperSize.handler({ Country: "India" })).resolves.toEqual("Unknown");
+});
--- lambda-typescript/test/check-age.test.ts
+++ lambda-typescript/test/check-age.test.ts
@@ -4,13 +4,13 @@ import * as CheckAge from '../lib/check-age';
 test('CheckAgeInGermany', () => {
     expect.assertions(1);
     expect(CheckAge.handler('{"queryStringParameters": {"Country": "Germany"}}')).resolves.toEqual(18);
+});
+test('CheckAgeInUSA', () => {
+    expect.assertions(1);
+    expect(CheckAge.handler("USA")).resolves.toEqual(21);
+});
 
-    test('CheckAgeInUSA', () => {
-        expect.assertions(1);
-        expect(CheckAge.handler("USA")).resolves.toEqual(21);
-        });
-
-        test('CheckAgeInIndia', () => {
-            expect.assertions(1);
-            expect(CheckAge.handler("India")).resolves.toEqual(18);
-            });
+test('CheckAgeInIndia', () => {
+    expect.assertions(1);
+    expect(CheckAge.handler("India")).resolves.toEqual(18);
+});
